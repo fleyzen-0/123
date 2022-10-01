20a2ad8de705883e866b4d3e666327baa67970d5
student <zherebniuk2017@gmail.com>
2022-10-01 11:08:30 +0300

    done lab_ex_5

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 number1 = int(input("Введіть два числа "))
 number2 = int(input())
-if number1%3!=0:
+if number1 > number2:
+    temp = number1
+    number1 = number2
+    number2 = temp
+
+if number1%2==0:
     number1 +=1
+
+
 while number1 <= number2:
     print(number1)
     number1 += 2
+
