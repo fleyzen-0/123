003ed640078e6e1c3a6e30b41a307d12e85b56b6
student <zherebniuk2017@gmail.com>
2022-10-01 11:40:22 +0300

    done hm_ex_2

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-number1 = int(input("Введіть два числа "))
+number1 = int(input("Введіть два числа - "))
 number2 = int(input())
 print("Усі числа діапазону -> ")
 while number1 <= number2:
     print(number1)
     number1 += 1
 
-
-
-print("Усі числа діапазону за спаданням ->")
+number1 = int(input("Введіть два числа - "))
+number2 = int(input())
+print("Усі числа діапазону за спаданням -> ")
+while number1 <= number2:
+    print(number2)
+    number2 -= 1
