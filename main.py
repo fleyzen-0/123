c6b599e1930fc147ead5bac15795ac73047ec441
student <zherebniuk2017@gmail.com>
2022-10-01 10:40:51 +0300

    done lab_ex_4

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 number1 = int(input("Введіть два числа "))
 number2 = int(input())
-while number1 <= number2:
+while number1 >= number2:
     print(number1)
+    number1 -= 1
+while number1 <= number2:
+    print(number2)
     number2 -= 1
